f0ed69b4f30cd94a56c2af71d287d0f9d5b3a07f
PeanutLager <56730480+Dominik-Alexander@users.noreply.github.com>
2024-12-01 18:31:40 +0100

    Personalize the standard qr-code output (#2)
    * Add sunflower
    * Change standard parameters for qr code generation
    * Assets folder

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from qr_code_generator import QRCodeGenerator
 
 
 def main():
-    qr_code_generator = QRCodeGenerator("Hello World")
-    qr_code_generator.generate_qr_code("Hello World")
+    qr_code_generator = QRCodeGenerator()
+    qr_code_generator.generate_qr_code()
 
 
 if __name__ == "__main__":
--- src/qr_code_generator.py
+++ src/qr_code_generator.py
@@ -1,23 +1,49 @@
 import qrcode
+from PIL import Image
 import loguru
 
 
 class QRCodeGenerator:
-    def __init__(self, string_to_convert: str):
-        self._string_to_convert = string_to_convert
-
-    def generate_qr_code(self, input: str) -> bool:
-        if input is None:
+    def generate_qr_code(
+        self,
+        input_str: str = "https://xn--grne-mhnesee-9ib3f.de/",
+        image: str = "assets/Sonnenblume_RGB_gelb.png",
+        format: str = "RGBA",
+        output: str = "data/qr_code.png",
+        fill_color: str = "#005437",
+        back_color: str = "#f2faf6",
+    ) -> bool:
+        if input_str is None:
             return False
         else:
             try:
-                img = qrcode.make(input)
+                icon = Image.open(image)
+                icon.thumbnail((icon.size[0] // 16, icon.size[1] // 16))
 
-                print(type(img))
-                print(img.size)
+                qr = qrcode.QRCode(
+                    version=1,
+                    error_correction=qrcode.constants.ERROR_CORRECT_H,
+                    box_size=10,
+                    border=4,
+                )
 
-                img.save("data/qr_code.png")
+                qr.add_data(input_str)
+                qr.make(fit=True)
+
+                img_qr = qr.make_image(fill_color=fill_color, back_color=back_color)
+
+                pos = (
+                    (img_qr.size[0] - icon.size[0]) // 2,
+                    (img_qr.size[1] - icon.size[1]) // 2,
+                )
+
+                img_qr = img_qr.convert(format)
+
+                temp_image = Image.new(format, img_qr.size)
+                temp_image.paste(img_qr, (0, 0))
+                temp_image.paste(icon, pos, icon)
+                temp_image.save(output)
                 return True
             except Exception as e:
-                loguru.logger.error(e)
+                loguru.logger.error(f"Error generating QR code: {e}")
                 return False
