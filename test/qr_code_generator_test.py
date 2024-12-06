5b89bd12da48baafd9542eb16669c40df3d64dc6
PeanutLager <56730480+Dominik-Alexander@users.noreply.github.com>
2024-12-06 21:23:20 +0100

    Features/ci (#3)
    * Flesh out ci-file
    
    * Fix error
    
    * Fix tests
    
    Constructor takes no arguments

--- test/qr_code_generator_test.py
+++ test/qr_code_generator_test.py
@@ -3,10 +3,10 @@ from qr_code_generator import QRCodeGenerator
 
 
 def test_generate_qr_code():
-    qr_code_generator = QRCodeGenerator("Hello World")
+    qr_code_generator = QRCodeGenerator()
     assert qr_code_generator.generate_qr_code("Hello World")
 
 
 def test_generate_qr_code_with_none():
-    qr_code_generator = QRCodeGenerator("Hello World")
+    qr_code_generator = QRCodeGenerator()
     assert not qr_code_generator.generate_qr_code(None)
